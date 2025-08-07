8e71e2507c1b69734aaf1d0cbca82432537851c6
SunX81 <152178254+SunX81@users.noreply.github.com>
2025-08-07 20:26:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ import os
 import time
 
 def compress_channel(channel, base_fraction):
-    # 进行DFT变换
+    # Perform DFT transform
     dft = np.fft.fft2(channel)
 
-    # 计算频谱的能量
+    # Calculate spectrum energy
     magnitude_spectrum = 20 * np.log(np.abs(dft) + 1)
 
-    # 计算自适应阈值
+    # Calculate spectrum energy
     threshold = np.percentile(magnitude_spectrum, (1 - base_fraction) * 100)
 
-    # 创建掩膜，保留大于阈值的频率成分
+    # Create a mask to retain frequency components above the threshold
     mask = np.where(magnitude_spectrum > threshold, 1, 0)
 
-    # 应用掩膜，丢弃低于阈值的频率成分
+    # Apply the mask to discard frequency components below the threshold
     dft = dft * mask
 
-    # 进行逆DFT变换
+    # Perform inverse DFT transform
     img_back = np.fft.ifft2(dft)
     img_back = np.abs(img_back)
     return img_back
@@ -29,11 +29,11 @@ def compress_image_with_adaptive_threshold(bgr_image, base_fraction):
 
     ycrcb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2YCrCb)
 
-    # 分离颜色通道
+    # Separate color channels
     b, g, r = cv2.split(bgr_image)
     Y, _, _ = cv2.split(ycrcb_image)
 
-    # 对每个颜色通道进行压缩
+    # Compress each color channel
     r_compressed = compress_channel(r, base_fraction)
     g_compressed = compress_channel(g, base_fraction)
     b_compressed = compress_channel(b, base_fraction)
@@ -68,10 +68,10 @@ def recompress_diff(imorig, gau_kz, sigma):
         g_Comparison = np.square(g_disp - g_compressed)
         r_Comparison = np.square(r_disp - r_compressed)
 
-        # 定义高斯滤波器
+        # Define a Gaussian filter
         gaussian_kernel = cv2.getGaussianKernel(gau_kz, sigma)  # 1D Gaussian kernel
         gaussian_kernel = gaussian_kernel * gaussian_kernel.T  # Make it 2D
-        # 应用高斯滤波器
+        # Apply the Gaussian filter
         Y_Comparison = cv2.filter2D(Y_Comparison, -1, gaussian_kernel)
         b_Comparison = cv2.filter2D(b_Comparison, -1, gaussian_kernel)
         g_Comparison = cv2.filter2D(g_Comparison, -1, gaussian_kernel)
@@ -133,13 +133,14 @@ def process_image_with_mask(impath, ksize, gau_kz, sigma):
     g_grayImage = cv2.resize(g_grayImage, (ori_width, ori_height))
     b_grayImage = cv2.resize(b_grayImage, (ori_width, ori_height))
 
+    # Apply the Median filter
     Y_grayImage = cv2.medianBlur(Y_grayImage, ksize)
     b_grayImage = cv2.medianBlur(b_grayImage, ksize)
     g_grayImage = cv2.medianBlur(g_grayImage, ksize)
     r_grayImage = cv2.medianBlur(r_grayImage, ksize)
-
+    
+    # Set the threshold
     th = 92
-    # 设置阈值
     threshold = np.percentile(Y_grayImage, th)
     thresh, Y_gray = cv2.threshold(Y_grayImage, threshold, maxval=255, type=cv2.THRESH_TOZERO)
 
@@ -154,10 +155,9 @@ def process_image_with_mask(impath, ksize, gau_kz, sigma):
 
     combined = np.where((Y_gray != 0) | (b_gray != 0) | (g_gray != 0) | (r_gray != 0), 1, 0)
 
-    # 使用高斯模糊函数对整张图像进行模糊处理
     blurred_image = cv2.GaussianBlur(im, (21, 21), 0)
 
-    # 创建一个结果图像，将模糊后的区域替换回原图
+    # Create a resulting image and replace the blurred area with the original
     result_image = im.copy()
     result_image[combined == 1] = blurred_image[combined == 1]
 
